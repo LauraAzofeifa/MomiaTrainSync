523334448296da753d6e46aa45430366a553b0f7
erick <107374899+erick01av@users.noreply.github.com>
2025-10-23 23:15:24 -0600

    Limpiando
    Se elimino el proyecto ya que se va a pasar a clean architecture.

--- Models/AtletaDashboard.cs
+++
@@ -1,9 +0,0 @@
-using MomiaTrainSync.Models;
-
-public class AtletaDashboard
-{
-    public string NombreAtleta { get; set; }
-    public RutinaItem RutinaDeHoy { get; set; }
-    public List<Disponibilidad> Disponibilidades { get; set; }
-    public List<ComentarioItem> Comentarios { get; set; }
-}
